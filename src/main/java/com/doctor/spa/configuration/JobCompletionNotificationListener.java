1496487ac3ad753c524596306aef4c745d8ef5f0
Thai Nguyen Q <nguyenquocthai7005@gmail.com>
2022-03-06 08:41:27 +0700

    spring batch

--- src/main/java/com/doctor/spa/configuration/JobCompletionNotificationListener.java
+++ src/main/java/com/doctor/spa/configuration/JobCompletionNotificationListener.java
@@ -3,7 +3,9 @@ package com.doctor.spa.configuration;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
 	  public JobCompletionNotificationListener() {
