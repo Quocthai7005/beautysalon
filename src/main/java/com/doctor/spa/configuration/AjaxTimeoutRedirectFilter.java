b2fbf7ae623efca63d8af92952491a4e27a73a74
Nguyen Quoc Thai <nguyenquocthai7005@gmail.com>
2023-05-15 22:09:33 +0700

    Refactor code

--- src/main/java/com/doctor/spa/configuration/AjaxTimeoutRedirectFilter.java
+++ src/main/java/com/doctor/spa/configuration/AjaxTimeoutRedirectFilter.java
@@ -20,98 +20,75 @@ import org.springframework.security.web.util.ThrowableAnalyzer;
 import org.springframework.security.web.util.ThrowableCauseExtractor;
 import org.springframework.web.filter.GenericFilterBean;
 
-public class AjaxTimeoutRedirectFilter extends GenericFilterBean
-{
+public class AjaxTimeoutRedirectFilter extends GenericFilterBean {
 
-	private static final Logger logger = LoggerFactory.getLogger(AjaxTimeoutRedirectFilter.class);
+    private static final Logger logger = LoggerFactory.getLogger(AjaxTimeoutRedirectFilter.class);
 
-	private ThrowableAnalyzer throwableAnalyzer = new DefaultThrowableAnalyzer();
-	private AuthenticationTrustResolver authenticationTrustResolver = new AuthenticationTrustResolverImpl();
+    private ThrowableAnalyzer throwableAnalyzer = new DefaultThrowableAnalyzer();
+    private AuthenticationTrustResolver authenticationTrustResolver = new AuthenticationTrustResolverImpl();
 
-	private int customSessionExpiredErrorCode = 901;
+    private int customSessionExpiredErrorCode = 901;
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
-	{
-		try
-		{
-			chain.doFilter(request, response);
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            chain.doFilter(request, response);
 
-			logger.debug("Chain processed normally");
-		}
-		catch (IOException ex)
-		{
-			throw ex;
-		}
-		catch (Exception ex)
-		{
-			Throwable[] causeChain = throwableAnalyzer.determineCauseChain(ex);
-			RuntimeException ase = (AuthenticationException) throwableAnalyzer.getFirstThrowableOfType(AuthenticationException.class, causeChain);
+            logger.debug("Chain processed normally");
+        } catch (IOException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            Throwable[] causeChain = throwableAnalyzer.determineCauseChain(ex);
+            RuntimeException ase = (AuthenticationException) throwableAnalyzer.getFirstThrowableOfType(AuthenticationException.class, causeChain);
 
-			if (ase == null)
-			{
-				ase = (AccessDeniedException) throwableAnalyzer.getFirstThrowableOfType(AccessDeniedException.class, causeChain);
-			}
+            if (ase == null) {
+                ase = (AccessDeniedException) throwableAnalyzer.getFirstThrowableOfType(AccessDeniedException.class, causeChain);
+            }
 
-			if (ase != null)
-			{
-				if (ase instanceof AuthenticationException)
-				{
-					throw ase;
-				}
-				else if (ase instanceof AccessDeniedException)
-				{
+            if (ase != null) {
+                if (ase instanceof AuthenticationException) {
+                    throw ase;
+                } else if (ase instanceof AccessDeniedException) {
 
-					if (authenticationTrustResolver.isAnonymous(SecurityContextHolder.getContext().getAuthentication()))
-					{
-						logger.info("User session expired or not logged in yet");
-						String ajaxHeader = ((HttpServletRequest) request).getHeader("X-Requested-With");
+                    if (authenticationTrustResolver.isAnonymous(SecurityContextHolder.getContext().getAuthentication())) {
+                        logger.info("User session expired or not logged in yet");
+                        String ajaxHeader = ((HttpServletRequest) request).getHeader("X-Requested-With");
 
-						if ("XMLHttpRequest".equals(ajaxHeader))
-						{
-							logger.info("Ajax call detected, send {} error code", this.customSessionExpiredErrorCode);
-							HttpServletResponse resp = (HttpServletResponse) response;
-							resp.sendError(this.customSessionExpiredErrorCode);
-						}
-						else
-						{
-							logger.info("Redirect to login page");
-							throw ase;
-						}
-					}
-					else
-					{
-						throw ase;
-					}
-				}
-			}
+                        if ("XMLHttpRequest".equals(ajaxHeader)) {
+                            logger.info("Ajax call detected, send {} error code", this.customSessionExpiredErrorCode);
+                            HttpServletResponse resp = (HttpServletResponse) response;
+                            resp.sendError(this.customSessionExpiredErrorCode);
+                        } else {
+                            logger.info("Redirect to login page");
+                            throw ase;
+                        }
+                    } else {
+                        throw ase;
+                    }
+                }
+            }
 
-		}
-	}
+        }
+    }
 
-	private static final class DefaultThrowableAnalyzer extends ThrowableAnalyzer
-	{
-		/**
-		 * @see org.springframework.security.web.util.ThrowableAnalyzer#initExtractorMap()
-		 */
-		protected void initExtractorMap()
-		{
-			super.initExtractorMap();
+    private static final class DefaultThrowableAnalyzer extends ThrowableAnalyzer {
+        /**
+         * @see org.springframework.security.web.util.ThrowableAnalyzer#initExtractorMap()
+         */
+        protected void initExtractorMap() {
+            super.initExtractorMap();
 
-			registerExtractor(ServletException.class, new ThrowableCauseExtractor()
-			{
-				public Throwable extractCause(Throwable throwable)
-				{
-					ThrowableAnalyzer.verifyThrowableHierarchy(throwable, ServletException.class);
-					return ((ServletException) throwable).getRootCause();
-				}
-			});
-		}
+            registerExtractor(ServletException.class, new ThrowableCauseExtractor() {
+                public Throwable extractCause(Throwable throwable) {
+                    ThrowableAnalyzer.verifyThrowableHierarchy(throwable, ServletException.class);
+                    return ((ServletException) throwable).getRootCause();
+                }
+            });
+        }
 
-	}
+    }
 
-	public void setCustomSessionExpiredErrorCode(int customSessionExpiredErrorCode)
-	{
-		this.customSessionExpiredErrorCode = customSessionExpiredErrorCode;
-	}
+    public void setCustomSessionExpiredErrorCode(int customSessionExpiredErrorCode) {
+        this.customSessionExpiredErrorCode = customSessionExpiredErrorCode;
+    }
 }
